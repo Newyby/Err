07e78786579af6c932ac92c26d27ed32c532aa97
Akinsanmi omobonike <officialraydata@gmail.com>
2024-02-14 18:49:13 +0100

    Add

--- app3.py
+++ app3.py
@@ -34,5 +34,5 @@ def compress2():
     return send_file(img_io, mimetype='image/jpeg', as_attachment=True, download_name='compressed.jpg')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
