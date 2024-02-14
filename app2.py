38983e6f230140b377ed3e8eb6b25f03a6e55bba
Akinsanmi omobonike <officialraydata@gmail.com>
2024-02-14 16:39:57 +0100

    Uodated 1

--- app2.py
+++ app2.py
@@ -2,7 +2,9 @@ from flask import Flask, request, send_file
 from PIL import Image
 from io import BytesIO
 
-app = Flask(__name__)                                                                                           @app.route('/')
+app = Flask(__name__)
+
+@app.route('/')
 def index():
     return app.send_static_file('second.html')
 
@@ -11,7 +13,10 @@ def compress():
     image = request.files['image']
     picture = Image.open(image)
     img_io = BytesIO()
-    picture.save(img_io, 'JPEG', optimize=True, quality=30)                                                         img_io.seek(0)                                          return send_file(img_io, mimetype='image/jpeg', as_attachment=True, download_name='compressed.jpg')
+    picture.save(img_io, 'JPEG', optimize=True, quality=30)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg', as_attachment=True, download_name='compressed.jpg')
 
 if __name__ == '__main__':
     app.run()
+
